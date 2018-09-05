871d612b1d205ecff64c95be1fa70059cadf3066
hjanott <hjanott@uos.de>
2018-09-05 17:46:57 +0200

    added own ceres_robot to project

--- ceres_navigation/scripts/wait_for_goal.py
+++ ceres_navigation/scripts/wait_for_goal.py
@@ -6,26 +6,27 @@ from geometry_msgs.msg import PoseStamped
 
 
 class WaitForGoal(smach.State):
+
     def __init__(self):
-        smach.State.__init__(
-            self,
-            outcomes=['succeeded', 'preempted'],
-            output_keys=['target_pose'])
-        self._global_target_pose = PoseStamped()
-        self._subscriber = None
-        self._flag_goal_received = False
+        smach.State.__init__(self,
+                             outcomes=['succeeded', 'preempted'],
+                             input_keys=['start_pose', 'waypoints', 'tolerance', 'use_start_pose'],
+                             output_keys=['start_pose', 'target_pose', 'waypoints', 'tolerance', 'use_start_pose'])
+        self.global_target_pose = PoseStamped()
+        self.subscriber = None
+        self.flag = 0
 
     def execute(self, userdata):
-        self._flag_goal_received = False
-        self._subscriber = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.goal_callback)
-
+        self.flag = 0
         rate = 0.3
-        while not self._flag_goal_received and not rospy.is_shutdown():
+        self.subscriber = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.goal_callback)
+
+        while not self.flag and not rospy.is_shutdown():    
             time.sleep(rate)
 
-        userdata.target_pose = self._global_target_pose
-        print "Target Pose:", self._global_target_pose.pose.position.x, self._global_target_pose.pose.position.y,\
-              self._global_target_pose.pose.position.z
+        userdata.target_pose = self.global_target_pose
+        print "Target Pose:", self.global_target_pose.pose.position.x, self.global_target_pose.pose.position.y,\
+              self.global_target_pose.pose.position.z
         if rospy.is_shutdown():
           return 'preempted'
 
@@ -33,6 +34,6 @@ class WaitForGoal(smach.State):
 
     def goal_callback(self, msg):
         print "Received goal:"
-        self._global_target_pose = msg
-        self._subscriber.unregister()
-        self._flag_goal_received = True
+        self.global_target_pose = msg
+        self.flag = 1
+        self.subscriber.unregister()
